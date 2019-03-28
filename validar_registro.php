e15aa931e0c133701ed36136f67f84637e2fdb3c
Alonso Heynes <alonso.heynes@gmail.com>
2019-03-28 16:02:42 -0600

    Sección de registro de usuarios completa. Función para el registro de eventos añadida. Prepared Statements utilizados para insertar datos a la base de datos. Registros repetidos prevenidos.

--- validar_registro.php
+++ validar_registro.php
@@ -1,40 +1,45 @@
 <?php include_once 'includes/templates/header.php' ?>
 
-<section class="seccion contenedor">
-  <h2>Resumen del registro</h2>
-
-
-  <?php
-    if(isset($_POST['submit'])):
-      $nombre = $_POST['nombre'];
-      $apellido = $_POST['apellido'];
-      $email = $_POST['emal'];
-      $regalo = $_POST['regalo'];
-      $total = $_POST['total_pedido'];
-      $fecha = date('Y-m-d H:i:s');
-
-      //pedidos
-      $boletos = $_POST['boletos'];
-      $camisas = $_POST['pedido_camisas'];
-      $etiquetas = $_POST['pedido_etiquetas'];
-
-      include_once 'includes/funciones/funciones.php';
-      $pedido = productos_json($boletos, $camisas, $etiquetas);
-      echo "<pre>";
-        var_dump($pedido);
-      echo "</pre>";
-
-
-
-
-
-      //echo "<pre>";
-      //  var_dump($_POST);
-      //echo "</pre>";
-    endif;
+  <?php if(isset($_POST['submit'])):
+  $nombre = $_POST['nombre'];
+  $apellido = $_POST['apellido'];
+  $email = $_POST['email'];
+  $regalo = $_POST['regalo'];
+  $total = $_POST['total_pedido'];
+  $fecha = date('Y-m-d H:i:s');
+  // Pedidos
+  $boletos = $_POST['boletos'];
+  $camisas = $_POST['pedido_camisas'];
+  $etiquetas = $_POST['pedido_etiquetas'];
+  include_once 'includes/funciones/funciones.php';
+  $pedido = productos_json($boletos, $camisas, $etiquetas);
+  $eventos = $_POST['registro'];
+  $registro = eventos_json($eventos);
+  try {
+    require_once('includes/funciones/db_conexion.php');
+    $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro, $regalo, $total);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    header('Location: validar_registro.php?exitoso=1');
+  } catch (Exception $e) {
+    $error = $e->getMessage();
+  }
   ?>
+<?php endif; ?>
+  <?php include_once 'includes/templates/header.php'; ?>
+  <section class="seccion contenedor">
+      <h2>Resumen Registro</h2>
 
-</section>
+     <?php if(isset($_GET['exitoso'])):
+           if($_GET['exitoso'] == "1"):
+                 echo "Registro exitoso";
+           endif;
+      endif; ?>
 
 
-<?php include_once 'includes/templates/footer.php' ?>
+
+
+  </section>
+  <?php include_once 'includes/templates/footer.php'; ?>
